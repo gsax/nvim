8decc1bc04f43e91038b17e26a200ff184bb34b8
Georg Sachs <gsx@protaisn.org>
2020-12-27 03:31:49 +0100

    Add clangd and ccls as C-Languageserver, use clangd

--- home/lua/lsp.lua
+++ home/lua/lsp.lua
@@ -7,6 +7,16 @@ local on_attach = function(client, bufnr)
   completion.on_attach(client, bufnr)
 end
 
+--the ccls c language server
+--lspconfig.ccls.setup{
+--  on_attach = on_attach;
+--}
+
+--the clangd c language server
+lspconfig.clangd.setup{
+  on_attach = on_attach;
+}
+
 --the go language server
 lspconfig.gopls.setup{
 	on_attach = on_attach
