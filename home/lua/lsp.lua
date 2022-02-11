308ede910be0e3497467d36968b854a15c2dc633
Georg Sachs <gsx@protaisn.org>
2022-02-11 11:10:03 +0100

    Change indentation for lua files to 3 spaces (luarocks styleguide)

--- home/lua/lsp.lua
+++ home/lua/lsp.lua
@@ -3,15 +3,15 @@ local lspconfig = require('lspconfig')
 
 --the ccls c language server
 --lspconfig.ccls.setup{
---  on_attach = on_attach;
+--   on_attach = on_attach;
 --}
 
 --the clangd c language server
 lspconfig.clangd.setup{
-  on_attach = on_attach;
+   on_attach = on_attach;
 }
 
 --the go language server
 lspconfig.gopls.setup{
-	on_attach = on_attach
+   on_attach = on_attach
 }
--- home/lua/treesitter.lua
+++ home/lua/treesitter.lua
@@ -1,12 +1,12 @@
 --enable treesitter
 require'nvim-treesitter.configs'.setup {
-  --enable treesitter highlighting for all languages
-  highlight = {
-    enable = true,
-    use_languagetree = false,
-  },
-  --enable treesitter indentation for all languages
-  indent = {
-    enable = true
-  },
+   --enable treesitter highlighting for all languages
+   highlight = {
+      enable = true,
+       use_languagetree = false,
+   },
+   --enable treesitter indentation for all languages
+   indent = {
+      enable = true
+   },
 }
