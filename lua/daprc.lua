ca26356df1f50376864ef07150b5da12ba2cf367
Georg Sachs <gsx@protaisn.org>
2022-07-08 22:01:33 +0200

    Use lldb for zig

--- lua/daprc.lua
+++ lua/daprc.lua
@@ -1,6 +1,6 @@
 local dap = require('dap')
 
--- configure lldb for c, cpp and rust
+-- configure lldb for c, cpp, rust and zig
 dap.adapters.lldb = {
    type = 'executable',
    command = '/usr/bin/lldb-vscode',
@@ -33,9 +33,10 @@ dap.configurations.c = {
    },
 }
 
--- use the same config for c, cpp and rust
+-- use the same config for c, cpp, rust and zig
 dap.configurations.cpp = dap.configurations.c
 dap.configurations.rust = dap.configurations.c
+dap.configurations.zig = dap.configurations.c
 
 -- load nvim-dap-go for go debugging with delve
 require('dap-go').setup()
