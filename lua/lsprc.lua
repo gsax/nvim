a3d2067dd9f52771962922098617d79e341e2ce6
Georg Sachs <gsx@protaisn.org>
2022-07-08 16:52:53 +0200

    Add zls (zig language server)

--- lua/lsprc.lua
+++ lua/lsprc.lua
@@ -133,7 +133,7 @@ end
 local capabilities = require('cmp_nvim_lsp').update_capabilities(
    vim.lsp.protocol.make_client_capabilities()
 )
-local servers = { 'clangd', 'eslint', 'vimls' }
+local servers = { 'clangd', 'eslint', 'vimls', 'zls' }
 for _, lsp in pairs(servers) do
    require('lspconfig')[lsp].setup({
       on_attach = on_attach,
