2bae761061835f5422b4b757302b7b4fd331d7c2
Georg Sachs <gsx@protaisn.org>
2022-10-16 00:55:22 +0200

    Replace deprecated function

--- lua/lsprc.lua
+++ lua/lsprc.lua
@@ -130,7 +130,7 @@ end
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
 -- capabilities comes from nvim-cmp
-local capabilities = require('cmp_nvim_lsp').update_capabilities(
+local capabilities = require('cmp_nvim_lsp').default_capabilities(
    vim.lsp.protocol.make_client_capabilities()
 )
 local servers = { 'clangd', 'eslint', 'julials', 'rust_analyzer', 'vimls', 'zls' }
