113aece710de8be4584c6b36c89c276a3f712c88
Georg Sachs <gsx@protaisn.org>
2022-12-23 01:33:53 +0100

    Use tabs for css, html, javascript and markdown

--- lua/format.lua
+++ lua/format.lua
@@ -2,7 +2,8 @@ vim.cmd([[
 augroup Autoformat
   autocmd!
   autocmd FileType c let &l:equalprg = 'clang-format --assume-filename %'
-  autocmd FileType css,html,javascript,markdown let &l:equalprg = 'prettier %'
+  autocmd FileType css,html,javascript,markdown let &l:equalprg =
+        \ 'prettier --use-tabs %'
   autocmd FileType go let &l:equalprg = 'goimports | gofumpt'
   autocmd FileType lua let &l:equalprg =
         \ 'stylua --config-path $XDG_CONFIG_HOME/stylua/stylua.toml -'
