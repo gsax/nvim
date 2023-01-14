2b1d7cec457b6cc93fd2c34da33402a239335309
Georg Sachs <gsx@protaisn.org>
2023-01-14 11:34:02 +0100

    Format init.lua

--- init.lua
+++ init.lua
@@ -3,12 +3,11 @@
 --scriptencoding utf-8
 
 -- map the leader key to space
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
+vim.g.mapleader = ' '
+vim.g.maplocalleader = '\\'
 
 -- first load all plugins, but load setup and config the plugins at last
 require('plugins')
-
 
 -- language providers
 --"""""""""""""""""""
@@ -24,11 +23,10 @@ vim.g.loaded_python3_provider = 0
 -- disable ruby provider
 vim.g.loaded_ruby_provider = 0
 
-
 -- appereance
 --"""""""""""
 -- set color of syntax highlight
-vim.o.background = "dark"
+vim.o.background = 'dark'
 
 -- always display status-line
 vim.o.laststatus = 2
@@ -37,32 +35,30 @@ vim.o.laststatus = 2
 vim.o.cursorline = true
 
 -- always show the gutterline
-vim.o.signcolumn = "yes"
+vim.o.signcolumn = 'yes'
 
 -- keep some lines distance to the top and bottom
 vim.o.scrolloff = 2
 
 -- activate TrueColor support
-if vim.fn.exists("+termguicolors") == 1 then
-  vim.opt.termguicolors = true
-elseif vim.fn.exists("+guicolors") == 1 then
-  vim.opt.guicolors = true
+if vim.fn.exists('+termguicolors') == 1 then
+   vim.opt.termguicolors = true
+elseif vim.fn.exists('+guicolors') == 1 then
+   vim.opt.guicolors = true
 end
 
 -- set colorscheme and activate background opacity
 require('dracularc')
 vim.cmd.colorscheme('dracula')
 
-
 -- split config
 --"""""""""""""
 -- vsplit for diffsplit
-vim.o.diffopt = "vertical"
+vim.o.diffopt = 'vertical'
 
 -- place splits right and under the current pane
 vim.o.splitbelow = true
 vim.o.splitright = true
-
 
 -- misc
 --"""""
@@ -76,7 +72,7 @@ vim.o.wildmenu = true
 vim.o.modeline = true
 
 -- make it possible to move outsitde the text in visual block mode
-vim.o.virtualedit = "block"
+vim.o.virtualedit = 'block'
 
 -- don't redraw the screen while executing macros
 vim.o.lazyredraw = true
@@ -91,7 +87,7 @@ vim.o.hidden = true
 vim.o.showmode = false
 
 -- disable mouse support
-vim.o.mouse = ""
+vim.o.mouse = ''
 
 -- use ripgrep as grepprg if installed
 vim.cmd([[
@@ -102,8 +98,15 @@ endif
 
 -- better listchars
 vim.opt.showbreak = '↪'
-vim.opt.listchars = { tab = '→·', space = '•', nbsp = '␣', trail = '…', eol = '↲', extends = '›', precedes = '‹' }
-
+vim.opt.listchars = {
+   tab = '→·',
+   space = '•',
+   nbsp = '␣',
+   trail = '…',
+   eol = '↲',
+   extends = '›',
+   precedes = '‹',
+}
 
 -- filetypes
 --""""""""""
@@ -111,8 +114,7 @@ vim.opt.listchars = { tab = '→·', space = '•', nbsp = '␣', trail = '…',
 vim.g.c_syntax_for_h = 1
 
 -- allow embedded syntax highlight for lua
-vim.g.vimsyn_embed = "lPr"
-
+vim.g.vimsyn_embed = 'lPr'
 
 -- plugin configuration
 --"""""""""""""""""""""
@@ -126,7 +128,7 @@ require('format')
 require('nerdcommenter')
 
 -- nvim-autopairs
-require('nvim-autopairs').setup{}
+require('nvim-autopairs').setup({})
 
 -- nvim-cmp
 require('cmprc')
@@ -138,7 +140,7 @@ require('daprc')
 require('diffviewrc')
 
 -- help-vsplit
-require('help-vsplit').setup{}
+require('help-vsplit').setup({})
 
 -- lsp
 require('lsprc')
@@ -153,7 +155,7 @@ require('luasniprc')
 require('neogitrc')
 
 -- nvim-surround
-require('nvim-surround').setup{}
+require('nvim-surround').setup({})
 
 -- telescope
 require('telescoperc')
