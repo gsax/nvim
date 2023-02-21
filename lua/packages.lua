b26fd82b8b9e181b1db18c38ecac051921a4e5b6
Georg Sachs <gsx@protaisn.org>
2023-02-21 20:10:19 +0100

    Add which-key.nvim

--- lua/packages.lua
+++ lua/packages.lua
@@ -161,4 +161,18 @@ require('dep')({
          require('nvim-surround').setup()
       end,
    },
+
+   -- UI
+   -- ^^
+   -- which-key.nvim: boom Create key bindings that stick. WhichKey is a lua
+   -- plugin for Neovim 0.5 that displays a popup with possible keybindings
+   -- of the command you started typing.
+   {
+      'folke/which-key.nvim',
+      function()
+         vim.o.timeout = true
+         vim.o.timeoutlen = 300
+         require('which-key').setup()
+      end,
+   },
 })
