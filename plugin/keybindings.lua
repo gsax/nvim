1de382bf66be2598ebf1182fb271ff49e4069576
Georg Sachs <gsx@protaisn.org>
2023-02-26 17:41:17 +0100

    Add some more telescope keybindings

--- plugin/keybindings.lua
+++ plugin/keybindings.lua
@@ -10,21 +10,40 @@ vim.keymap.set(
    'n',
    '<leader>l',
    require('telescope.builtin').find_files,
-   { desc = 'Search all Files' }
+   { desc = '[l] search all files' }
 )
-
 -- search for files under source control
 vim.keymap.set(
    'n',
    '<leader>p',
    require('telescope.builtin').git_files,
-   { desc = 'Search Files in git' }
+   { desc = '[p] search files in git' }
 )
-
 -- search for open buffers
 vim.keymap.set(
    'n',
    '<leader>b',
    require('telescope.builtin').buffers,
-   { desc = 'Find existing buffers' }
+   { desc = '[b] find existing buffers' }
+)
+-- find recently opened files
+vim.keymap.set(
+   'n',
+   '<leader>?',
+   require('telescope.builtin').oldfiles,
+   { desc = '[?] find recently opened files' }
+)
+-- search helpfiles
+vim.keymap.set(
+   'n',
+   '<leader>sh',
+   require('telescope.builtin').help_tags,
+   { desc = '[s]earch [h]elp' }
+)
+-- search lsp diagnostices
+vim.keymap.set(
+   'n',
+   '<leader>sd',
+   require('telescope.builtin').diagnostics,
+   { desc = '[s]earch [d]iagnostics' }
 )
