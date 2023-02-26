f363de6add8b15a7274d74111a1e086ab43e8f5a
Georg Sachs <gsx@protaisn.org>
2023-02-26 18:03:19 +0100

    Move dap keybindings from dap.lua to keybindings.lua

--- plugin/keybindings.lua
+++ plugin/keybindings.lua
@@ -4,7 +4,12 @@ vim.keymap.set('c', '<C-U>', '<C-E><C-U>')
 -- leave terminal mode with ESC
 vim.keymap.set('t', '<Esc>', '<C-\\><C-n>')
 
--- keybindings for telescope
+-- set catagories for which-key
+local wk = require('which-key')
+wk.register({ s = { name = '[s] telescope' } }, { prefix = '<leader>' })
+wk.register({ d = { name = '[d] debugger' } }, { prefix = '<leader>' })
+
+-- telescope
 -- search for files in the current directory
 vim.keymap.set(
    'n',
@@ -46,4 +51,79 @@ vim.keymap.set(
    '<leader>sd',
    require('telescope.builtin').diagnostics,
    { desc = '[s]earch [d]iagnostics' }
+)
+
+-- debug adapter
+vim.keymap.set(
+   'n',
+   '<F2>',
+   require('dapui').toggle,
+   { desc = 'toggle debugger' }
+)
+vim.keymap.set(
+   'n',
+   '<F5>',
+   require('dap').continue,
+   { desc = 'debugger continue' }
+)
+vim.keymap.set(
+   'n',
+   '<F6>',
+   require('dap').toggle_breakpoint,
+   { desc = 'debugger toggle breakpiont' }
+)
+vim.keymap.set(
+   'n',
+   '<F10>',
+   require('dap').step_over,
+   { desc = 'debugger step over' }
+)
+vim.keymap.set(
+   'n',
+   '<F11>',
+   require('dap').step_into,
+   { desc = 'debugger step into' }
+)
+vim.keymap.set(
+   'n',
+   '<F12>',
+   require('dap').step_out,
+   { desc = 'debugger step out' }
+)
+
+vim.keymap.set(
+   'n',
+   '<leader>dt',
+   require('dapui').toggle,
+   { desc = '[d]ebugger [t]oggle' }
+)
+vim.keymap.set(
+   'n',
+   '<leader>db',
+   require('dap').toggle_breakpoint,
+   { desc = '[d]ebugger toggle [b]reakpiont' }
+)
+vim.keymap.set(
+   'n',
+   '<leader>dc',
+   require('dap').continue,
+   { desc = '[d]ebugger [c]ontinue' }
+)
+vim.keymap.set(
+   'n',
+   '<leader>dn',
+   require('dap').step_over,
+   { desc = '[d]ebugger step over/[n]ext' }
+)
+vim.keymap.set(
+   'n',
+   '<leader>ds',
+   require('dap').step_into,
+   { desc = '[d]ebugger step into/[s]tep' }
+)
+vim.keymap.set(
+   'n',
+   '<leader>df',
+   require('dap').step_out,
+   { desc = '[d]ebugger step out/[f]inish' }
 )
