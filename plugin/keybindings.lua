b0f8600515f50d37e3e103b987baa073257539b0
Georg Sachs <gsx@protaisn.org>
2023-02-26 17:29:21 +0100

    Move telescope keybindings from telescope.lue to keybindings.lua

--- plugin/keybindings.lua
+++ plugin/keybindings.lua
@@ -3,3 +3,28 @@ vim.keymap.set('c', '<C-U>', '<C-E><C-U>')
 
 -- leave terminal mode with ESC
 vim.keymap.set('t', '<Esc>', '<C-\\><C-n>')
+
+-- keybindings for telescope
+-- search for files in the current directory
+vim.keymap.set(
+   'n',
+   '<leader>l',
+   require('telescope.builtin').find_files,
+   { desc = 'Search all Files' }
+)
+
+-- search for files under source control
+vim.keymap.set(
+   'n',
+   '<leader>p',
+   require('telescope.builtin').git_files,
+   { desc = 'Search Files in git' }
+)
+
+-- search for open buffers
+vim.keymap.set(
+   'n',
+   '<leader>b',
+   require('telescope.builtin').buffers,
+   { desc = 'Find existing buffers' }
+)
